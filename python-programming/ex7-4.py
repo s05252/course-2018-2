45cc39b76f91ad25a81e952d71aff6cb49bab1f2
SuJin Park <s05252@cu.ac.kr>
2018-11-23 14:39:35 +0900

    add ex7-4.py

--- python-programming/ex7-4.py
+++ python-programming/ex7-4.py
@@ -5,8 +5,8 @@ kim, fail, fail
 shin, pass, fail
 """
 
-n = re.compile("(\w{3})[,](\w{4})[,](\w{4})")
-print(n.sub("\g<1>, \g<3>, pass", data))
+n = re.compile("(\w+[,]\w+[,])\w+")
+print(n.sub("\g<1>pass", data))
 
 n2 = re.compile("fail$",re.MULTILINE)
 print(n2.sub('pass',data))
